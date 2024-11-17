ccc653cf8185d666578ca638c6b7af9280ece113
AnaisChapon <167913861+AnaisChapon@users.noreply.github.com>
2024-11-17 23:19:54 +0100

    Update dashboard_complet_light.py
    modif fichier csv

--- dashboard_complet_light.py
+++ dashboard_complet_light.py
@@ -12,7 +12,7 @@ current_directory = os.path.dirname(os.path.abspath(__file__))
 #path_df_train_url = "https://raw.githubusercontent.com/AnaisChapon/dashboard_ocr7/refs/heads/main/application_test_light.csv"
 #path_definition_features_df_url = "https://raw.githubusercontent.com/AnaisChapon/dashboard_ocr7/refs/heads/main/definition_features.csv"
 path_definition_features_df = os.path.join(current_directory, "definition_features.csv")
-path_df_train = os.path.join(current_directory, "df_train.csv")
+path_df_train = os.path.join(current_directory, "application_test_light.csv")
 
 #definition_features_df = pd.read_csv(path_definition_features_df_url)
 #df_train = pd.read_csv(path_df_train_url, low_memory=True)
