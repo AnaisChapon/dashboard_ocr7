6e040c234186021572761b923b82659bd81c3827
AnaisChapon <167913861+AnaisChapon@users.noreply.github.com>
2024-11-29 17:59:00 +0100

    Update CHAPON_Anais_1_dashboard_et_API_102024.py
    changement chemin API et dashboard

--- CHAPON_Anais_1_dashboard_et_API_102024.py
+++ CHAPON_Anais_1_dashboard_et_API_102024.py
@@ -9,9 +9,9 @@ api_port = "5000"
 dashboard_port = "8000"
 
 # Lancer l'API Flask
-api_process = subprocess.Popen(["python", os.path.join(scripts_directory, "api_complet_light.py")], env=dict(os.environ, PORT=api_port))
+api_process = subprocess.Popen(["python", "api_complet_light.py"], env=dict(os.environ, PORT=api_port))
 print(f"API Flask lancée sur le port {api_port}")
 
 # Lancer le dashboard Streamlit
-dashboard_process = subprocess.Popen(["streamlit", "run", os.path.join(scripts_directory, "dashboard_complet_light.py"), "--server.port", dashboard_port])
+dashboard_process = subprocess.Popen(["streamlit", "run", "dashboard_complet_light.py", "--server.port", dashboard_port])
 print(f"Dashboard Streamlit lancé sur le port {dashboard_port}")
