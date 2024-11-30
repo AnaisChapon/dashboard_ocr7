e28468777d1650165822bd1681af870a559402ec
AnaisChapon <167913861+AnaisChapon@users.noreply.github.com>
2024-11-30 23:35:01 +0100

    Update CHAPON_Anais_1_dashboard_et_API_102024.py
    changement définition api_port et dashboard_port

--- CHAPON_Anais_1_dashboard_et_API_102024.py
+++ CHAPON_Anais_1_dashboard_et_API_102024.py
@@ -2,10 +2,11 @@ import subprocess
 import os
 
 # Définir le port pour l'API Flask et le dashboard Streamlit
-api_port = "5000"
-dashboard_port = "8001"
+api_port = os.environ.get("PORT", "5000")
+dashboard_port = os.environ.get("PORT", "8000")
 
 # Lancer l'API Flask
+#api_process = subprocess.Popen(["python", "api_complet_light.py"], env=dict(os.environ, PORT=api_port))
 api_process = subprocess.Popen(["python", "api_complet_light.py"], env=dict(os.environ, PORT=api_port))
 print(f"API Flask lancée sur le port {api_port}")
 
