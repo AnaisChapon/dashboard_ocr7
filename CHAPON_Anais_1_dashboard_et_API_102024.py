b3291abbfa0d2fc77064d2b67cd12a2c0e910a27
AnaisChapon <167913861+AnaisChapon@users.noreply.github.com>
2024-11-30 22:54:12 +0100

    Update CHAPON_Anais_1_dashboard_et_API_102024.py
    ajout de "--server.address", "0.0.0.0" au dashboard_process

--- CHAPON_Anais_1_dashboard_et_API_102024.py
+++ CHAPON_Anais_1_dashboard_et_API_102024.py
@@ -10,5 +10,5 @@ api_process = subprocess.Popen(["python", "api_complet_light.py"], env=dict(os.e
 print(f"API Flask lancée sur le port {api_port}")
 
 # Lancer le dashboard Streamlit
-dashboard_process = subprocess.Popen(["streamlit", "run", "dashboard_complet_light.py", "--server.port", dashboard_port])
+dashboard_process = subprocess.Popen(["streamlit", "run", "dashboard_complet_light.py", "--server.port", dashboard_port,"--server.address", "0.0.0.0"])
 print(f"Dashboard Streamlit lancé sur le port {dashboard_port}")
